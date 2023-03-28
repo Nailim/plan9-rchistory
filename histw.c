4f3b9e718c671566c96c6c3cefcfa793620111d8
Nailim <jernej.kranjec@gmail.com>
2023-03-28 11:27:52 +0000

    Code cleanup
    Reduce number of IF checks and lines of code.

--- histw.c
+++ histw.c
@@ -862,13 +862,9 @@ process(char *s)
 		}
 
 		/* reset history tracking and state if command entered or canceled */
-		if(*s == 'c' && r == 10){
-			/* enter key */
-			hop = 0;
-			resethstate();
-		}
-		if(*s == 'c' && r == 127){
-			/* delete key */
+		if(*s == 'c' && (r == 10 || r == 127)){
+			/* 10 - enter key */
+			/* 127 - delete key */
 			hop = 0;
 			resethstate();
 		}
